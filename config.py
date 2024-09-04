b5e1369e43b78bc057dd7ac33cfdd8bc980b8a53
AshwingamerYt <ashwinpatil2005@gmail.com>
2024-09-04 21:22:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7470411779:AAHrHYYzQhmqYUAXw9mTuoK6VX8PBcNk2yo")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("AshwingamerYtb")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5887426657")
 
 
 
